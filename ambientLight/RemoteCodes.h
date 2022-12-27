34786ccdd445108a8d6f7805a9e106c5d6b17c15
Plopez <iespaulopez@gmail.com>
2022-12-27 14:32:08 +0100

    IR send + remote codes define file

---
+++ ambientLight/RemoteCodes.h
@@ -0,0 +1,24 @@
+#define BRIGHT_UP 0x45
+#define BRIGHT_DOWN 0x46
+
+#define OFF 0x47
+#define ON 0x43
+
+#define PARTY 0x44
+#define RAINBOW 0x40
+
+#define PRESET_1 0x7
+#define PRESET_2 0x15
+
+#define TIME 0x9
+
+#define RED 0x16
+#define GREEN 0x19
+#define BLUE 0xD
+
+#define SPECTRUM_RED 0xC
+#define SPECTRUM_YELLOW 0x5E
+#define SPECTRUM_BLUE 0x42
+#define SPECTRUM_GREEN 0x4A
+
+#define WHITE 0x1C
--- ambientLight/ambientLight.ino
+++ ambientLight/ambientLight.ino
@@ -1,9 +1,21 @@
+#include <IRremote.h>
+
+#include "RemoteCodes.h"
+
+#define IR_RECEIVE_PIN 4
+#define IR_SEND_PIN 2
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial.print(F("Ready to send IR signals at pin "));
+  Serial.println(IR_SEND_PIN);
+  
+  IrSender.begin(IR_SEND_PIN);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  IrSender.sendNEC(0, BRIGHT_UP, 1);
+  Serial.println("Sending");
+  delay(1000);
 }
